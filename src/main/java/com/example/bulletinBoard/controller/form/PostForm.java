40430d058e3e016868f2dd069d390498ab3bcc4c
디지털CRM팀/최재량 책임 <jaeryang@hyundai-autoever.com>
2024-10-07 17:37:46 +0900

    bean validation 적용

--- src/main/java/com/example/bulletinBoard/controller/form/PostForm.java
+++ src/main/java/com/example/bulletinBoard/controller/form/PostForm.java
@@ -3,15 +3,22 @@ package com.example.bulletinBoard.controller.form;
 import com.example.bulletinBoard.domain.Category;
 import com.example.bulletinBoard.domain.Member;
 import com.example.bulletinBoard.domain.Post;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 
 @Getter
 @Setter
 public class PostForm {
+    @NotBlank
+    @Length(max = 15)
     private String title;
+    @NotBlank
     private String content;
+    @NotNull
     private Long categoryId;
     private Character secretYN = 'N';
 
